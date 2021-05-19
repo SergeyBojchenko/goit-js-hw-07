49a111ca0035f22a2bdf1de5ac2676694342f365
Sergey Bojchenko <bojchenkosergey@gmail.com>
2021-05-19 14:44:15 +0300

    DZ_7 последние правки

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,16 +1,13 @@
-const refs = {
-    validationInputRef: document.querySelector('#validation-input'),
-    minLengthWord: 6,
-};
-console.log(refs.validationInputRef);
-refs.validationInputRef.addEventListener('blur', onLossOfFocus);
+const validationInputRef = document.querySelector('#validation-input');
+const minLengthWord = validationInputRef.dataset.length;
+
+validationInputRef.addEventListener('blur', onLossOfFocus);
 
 function onLossOfFocus(event) {
     console.dir(event.currentTarget.value.length);
-    if (event.currentTarget.value.length < refs.minLengthWord) {
-        refs.validationInputRef.classList.add('invalid');
+    if (event.currentTarget.value.length < minLengthWord) {
+        validationInputRef.classList='invalid';
     } else {
-        refs.validationInputRef.classList.add('valid');
-        refs.validationInputRef.classList.remove('invalid')
+        validationInputRef.classList='valid';
     };
 };
