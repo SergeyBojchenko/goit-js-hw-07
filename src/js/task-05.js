881e4d4305737ea0b9610cce2abc5b8ed61269e5
Sergey Bojchenko <bojchenkosergey@gmail.com>
2021-05-20 23:48:55 +0300

    DZ_7 mvp

--- src/js/task-05.js
+++
@@ -1,9 +0,0 @@
-const nameInputRef = document.querySelector('#name-input');
-const nameOutputRef = document.querySelector('#name-output');
-
-nameInputRef.addEventListener('input', onInputChange);
-
-function onInputChange(event) {
-    console.log(event.currentTarget.value);
-     nameOutputRef.textContent = event.currentTarget.value;
-};
--- src/js/task-07.js
+++
@@ -1,8 +0,0 @@
-const fontSizeControlRef = document.querySelector('#font-size-control');
-const textRef = document.querySelector('#text');
-
-fontSizeControlRef.addEventListener('input', onChangeFontSize);
-
-function onChangeFontSize(event) {
-    textRef.style.fontSize = event.currentTarget.value + 'px';
-};
